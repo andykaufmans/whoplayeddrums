5cc0367eaf6edf14d402c058d89e085d5556fdb9
andykaufmans <kevin@kevinhood.com>
2026-05-02 11:09:22 -0400

    Remove unused old_src backup folder

--- old_src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as DrummerCard } from './DrummerCard';
-export { default as YouTubeEmbed } from './YouTubeEmbed';
-export { VideoGrid } from './YouTubeEmbed';
